c4e294cbbe41d8b2afe6a8b1241c5f883349f89b
frs-marcelle <89610805+frs-marcelle@users.noreply.github.com>
2021-11-08 17:03:35 -0300

    Add files via upload

---
+++ js/CannonBall.js
@@ -0,0 +1,34 @@
+class CannonBall {
+  constructor(x, y) {
+    var options = {
+      restitution: 0.8,
+      friction: 1.0,
+      density: 1.0,
+      isStatic: true
+    };
+    this.r = 40;
+//crie um corpo circular
+//carregue a imagem
+    World.add(world, this.body);
+  }
+
+  shoot() {
+    
+    //defina o ângulo da bala para o canhão
+    //defina valor estático para o corpo
+    //defina a velocidade da bala para fazer a bala se mover
+  }
+
+  display() {
+    var angle = this.body.angle;
+    var pos = this.body.position;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+//mostrar a imagem
+    pop();
+
+    }
+  }
+
